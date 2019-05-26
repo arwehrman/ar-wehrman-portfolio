309eab72bcb5e2af7a9fee774b9af617ff231677
Amanda Wehrman <arwehrman@Amandas-MBP.fios-router.home>
2019-05-26 15:52:31 -0400

    added project and about components

---
+++ src/components/facts.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Facts = () => (
+    <div>
+<h2>Skills</h2>
+        <div className="skillBlock">
+            <h3>Core</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>SQL</li>
+                <li>Ruby</li>
+            </ul>
+        </div>
+        <div className="skillBlock">
+            <h3>Libaries</h3>
+            <ul>
+                <li>React</li>
+                <li>Wordpress</li>
+                <li>Gatsby</li>
+            </ul>
+        </div>
+            
+
+        <div className="resume">
+            <h1>Resume</h1>
+            <p>Space for resume</p>
+        </div>
+    </div>
+   
+)
+
+export default Facts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,54 @@
-import React from "react"
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Layout from '../components/layout';
+import ProjectPreview from '../components/project-preview';
+import About from '../components/about'
+import Facts from '../components/facts'
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allProjectsJson {
+        edges {
+          node {
+            title
+            slug
+            description
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const projects = data.allProjectsJson.edges;
 
+  return (
+    <Layout>
+      <About />
+      {projects.map(({ node: project }) => {
+        const title = project.title;
+        const description = project.description;
+        const slug = project.slug;
+        const imageData = project.image.childImageSharp.fluid;
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+        return (
+          <ProjectPreview
+            title={title}
+            description={description}
+            slug={slug}
+            imageData={imageData}
+          />
+        );
+      })}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    
-  </Layout>
-)
+      <Facts />
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default Home;
