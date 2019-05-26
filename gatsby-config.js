ddfdd2119c08147815db69d896b914bea7960886
Amanda Wehrman <arwehrman@Amandas-MBP.fios-router.home>
2019-05-26 14:43:01 -0400

    updated config file for JSON data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AR Wehrman`,
+    description: `Amanda R. Wehrman's portfolio site`,
+    author: `Amanda R. Wehrman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,7 +10,15 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'project',
+        path: './data',
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,11 +32,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ARW_logo.png`, 
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    },   
   ],
 }
