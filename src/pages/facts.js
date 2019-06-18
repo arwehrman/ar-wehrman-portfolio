457be2798de813a6e935199c1efd5203e9a3d7d9
arwehrman <arwehrman@gmail.com>
2019-06-17 21:09:46 -0400

    hashing out CSS for resume section

--- src/pages/facts.js
+++ src/pages/facts.js
@@ -6,77 +6,79 @@ const Facts = () => (
     <div>
       <h2>Skills</h2>
       <div className="skills">
-      <div className="skillBlock">
-        <h3>Core</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>SQL</li>
-          <li>Ruby</li>
-        </ul>
+        <div className="skillBlock">
+          <h3>Core</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>SQL</li>
+            <li>Ruby</li>
+          </ul>
+        </div>
+        <div className="skillBlock">
+          <h3>Libaries</h3>
+          <ul>
+            <li>React</li>
+            <li>Wordpress</li>
+            <li>Gatsby</li>
+          </ul>
+        </div>
+        <div className="skillBlock">
+          <h3>Essential</h3>
+          <ul>
+            <li>Organization</li>
+            <li>Peristence</li>
+            <li>Self-Motivated</li>
+          </ul>
+        </div>
       </div>
-      <div className="skillBlock">
-        <h3>Libaries</h3>
-        <ul>
-          <li>React</li>
-          <li>Wordpress</li>
-          <li>Gatsby</li>
-        </ul>
-      </div>
-      <div className="skillBlock">
-        <h3>Essential</h3>
-        <ul>
-          <li>Organization</li>
-          <li>Peristence</li>
-          <li>Self-Motivated</li>
-        </ul>
-      </div>
-      </div>
-      
 
       <div className="resume">
         <h2>Job Experience</h2>
-        <h3>IT Manager</h3>
-        <p>Pennsylvania Convention Center, Philadelphia, PA</p>
-        <p>December 2010 to present</p>
-        <ul>
-          <li>
-            configure, write custom Crystal reports, and train users on
-            Ungerboeck, event management software
-          </li>
-          <li>
-            conducted quality control testing for Ungerboeck, event management
-            software upgrade, allowing for smooth transition for users between
-            versions
-          </li>
-          <li>
-            project lead on two website remodels 
-          </li>
-          <li>
-            web adminstrator for the companies internal and external sites
-          </li>
-          <li>
-            scheduled and managed outside vendors on projects, reducing the
-            amount of downtime for clients and staff
-          </li>
-          <li>
-            re-designed and implemented cost model for digital signage system,
-            leading to an increase in revenue
-          </li>
-        </ul>
+        <div className="job">
+          <h3>IT Manager</h3>
+          <p>Pennsylvania Convention Center, Philadelphia, PA</p>
+          <p>December 2010 to present</p>
+          <ul>
+            <li>
+              configure, write custom Crystal reports, and train users on
+              Ungerboeck, event management software
+            </li>
+            <li>
+              conducted quality control testing for Ungerboeck, event management
+              software upgrade, allowing for smooth transition for users between
+              versions
+            </li>
+            <li>project lead on two website remodels</li>
+            <li>
+              web adminstrator for the companies internal and external sites
+            </li>
+            <li>
+              scheduled and managed outside vendors on projects, reducing the
+              amount of downtime for clients and staff
+            </li>
+            <li>
+              re-designed and implemented cost model for digital signage system,
+              leading to an increase in revenue
+            </li>
+          </ul>
+        </div>
 
-        <h3>Museum Photographer</h3>
-        <p>Kennedy Museum of Art, Athens, OH</p>
-        <p>September 2009 to June 2010</p>
-        <ul>
-          <li>photographed artwork for museum’s inventory database</li>
-          <li>assisted in the installation of visiting artist exhibits</li>
-          <li>
-            photographed exhibits and openings for the museum’s website and
-            other promotional material
-          </li>
-        </ul>
+        <div className="job">
+          <h3>Museum Photographer</h3>
+          <p>Kennedy Museum of Art, Athens, OH</p>
+          <p>September 2009 to June 2010</p>
+          <ul>
+            <li>photographed artwork for museum’s inventory database</li>
+            <li>assisted in the installation of visiting artist exhibits</li>
+            <li>
+              photographed exhibits and openings for the museum’s website and
+              other promotional material
+            </li>
+          </ul>
+        </div>
+
         <h2>Education</h2>
         <p>Flatiron School - 2018</p>
         <p>
