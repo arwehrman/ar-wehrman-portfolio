8bd83afa3c5ab9392708f6e2bf7ca0b9f7b6782c
arwehrman <arwehrman@gmail.com>
2019-06-24 21:35:40 -0400

    added more to Hobbies

--- src/pages/hobbies.js
+++ src/pages/hobbies.js
@@ -6,18 +6,18 @@ const Hobbies = () => (
     <Layout>
         <div>
             <h1>Hobbies</h1>
-        </div>
-        <div>
-            <br></br>
+        
+        <div className="hobby">
             <img src={wonder} alt="Wonder Wehrman" />
-            <p>Roller Derby</p>
-                <ul className="hobbies-list">
-                    <li>Player- 3 years</li>
-                    <li>Announcer- 2 years</li>
-                    <li>Photographer- 1 year</li>
-                    <li>WFTDA Volunteer- worked as Clips Media Manager for WFTDA Broadcast</li>
-                </ul>
-            
+            <p>Since 2010 I've been involved with Philly Roller Derby. I've been an announcer, photographer, volunteer and skater. In 2017 I was in charge of planning and organize our leagues annual 3 day tournament, East Coast Derby Extravaganza. This tournament hosted leagues from all over the United States and internationally. I cordinated teams, officials, photographers, vendors, and created game schedules.  I also worked as the Clips Media Manager for WFTDA Broadcast, which involved creating clips from broadcast footage to be used in commericials and media coverage.</p>
+        </div>
+
+        <div className="hobby">
+            <p>I love to travel within the United States and internationally. I've been to France, Ireland, and Argentina. I'm currently planning a trip to China.</p>
+        </div>
+        <div className="hobby">
+            <p>I really enjoy photography. I started building cameras out of Legos when I was 4 years old. I would pretend that I was taking pictures and then I would go to my "dark room" and draw the pictures I took. My favorite photographers are Robert Frank and Sylvia Placky</p>
+        </div>
         </div>
     </Layout>
     
